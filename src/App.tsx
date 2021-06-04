c686bb53fc3a7d064d7267a5b93807661f9bd956
허예진 <yejin@ibleaders.co.kr>
2021-06-04 17:35:56 +0900

    [#] ibsheet-loader typescript 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import loader from '@ibsheet/loader';
 
 interface Props {}
 
+loader.config({
+  registry: [{
+    name: 'ibsheet',
+    baseUrl: 'assets/ibsheet'
+  }]
+})
 const App = ({  }: Props) => {
-  return <h1>IBSheet Loader Samples</h1>;
+  // 페이지 종료 시 로더 제거
+  useEffect (() => {
+    return (
+      loader.unload()
+    )
+  }, []);
+
+  return (
+    <>
+      <h1>IBSheet Loader Samples</h1>
+    </>
+  )
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
